158348d0587e3363243b00fa1d6fd4303bd35d09
Nico Watine <nico@studioncreations.com>
2020-04-01 13:34:17 -0500

    Add a white border around imgs

--- src/pages/index.php
+++ src/pages/index.php
@@ -16,4 +16,8 @@ body-class: index
 		<div class="img-column"><img src="/img/galleries/1x1/4.jpg" alt=""></div>
 		<div class="img-column"><img src="/img/galleries/1x1/5.jpg" alt=""></div>
 	</div>
+	<div class="row collapse">
+		<div class="medium-6 columns"><img src="/img/galleries/4x3/1.jpg" alt=""></div>
+		<div class="medium-6 columns"><img src="/img/galleries/4x3/2.jpg" alt=""></div>
+	</div>
 </section>
