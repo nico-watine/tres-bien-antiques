e75635ae3bfc0b18134b86f72c937ce425e37642
Nico Watine <nico@studioncreations.com>
2020-04-01 13:31:00 -0500

    Insert first 5 img

--- src/pages/index.php
+++ src/pages/index.php
@@ -10,10 +10,10 @@ body-class: index
 ---
 <section class="photo-gallery">
 	<div class="row row-of-5">
-		<div class="img-column">&amp;</div>
-		<div class="img-column">&amp;</div>
-		<div class="img-column">&amp;</div>
-		<div class="img-column">&amp;</div>
-		<div class="img-column">&amp;</div>
+		<div class="img-column"><img src="/img/galleries/1x1/1.jpg" alt=""></div>
+		<div class="img-column"><img src="/img/galleries/1x1/2.jpg" alt=""></div>
+		<div class="img-column"><img src="/img/galleries/1x1/3.jpg" alt=""></div>
+		<div class="img-column"><img src="/img/galleries/1x1/4.jpg" alt=""></div>
+		<div class="img-column"><img src="/img/galleries/1x1/5.jpg" alt=""></div>
 	</div>
 </section>
