af695d455393dc2385e51e11d7c3eda31b6fa7c3
Nico Watine <nico@studioncreations.com>
2020-04-01 13:30:10 -0500

    Add imgs create row of 5 with no gutters via scss

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,3 +8,12 @@ og-image:
 body-id: index
 body-class: index
 ---
+<section class="photo-gallery">
+	<div class="row row-of-5">
+		<div class="img-column">&amp;</div>
+		<div class="img-column">&amp;</div>
+		<div class="img-column">&amp;</div>
+		<div class="img-column">&amp;</div>
+		<div class="img-column">&amp;</div>
+	</div>
+</section>
