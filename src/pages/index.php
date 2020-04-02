c52795faa16419728d2e75e7ad454fa7f72341af
Nico Watine <nico@studioncreations.com>
2020-04-02 11:05:17 -0500

    Restore

--- src/pages/index.php
+++ src/pages/index.php
@@ -10,8 +10,8 @@ body-class: index
 ---
 <section class="index-splash" style="margin-bottom: 80px;">
 	<div class="text-block">
-		<h1 class="headline font-weight-600 text-white text-uppercase">Très Bien Antiques</h1>
-		<h2 class="subhead font-weight-700 text-white text-uppercase">Direct importer and Curator of French and European Antiques in Houston, Texas</h2>
+		<h1 class="headline">Très Bien Antiques</h1>
+		<h2 class="subhead">Direct importer and Curator of French and European Antiques in Houston, Texas</h2>
 	</div>
 </section>
 <section class="photo-gallery" style="margin-bottom: 120px;">
