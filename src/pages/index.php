57787380344de7931f7cb41fc67a0370424683b2
Nico Watine <nico@studioncreations.com>
2020-04-08 10:38:08 -0500

    Insert her photo

--- src/pages/index.php
+++ src/pages/index.php
@@ -14,6 +14,16 @@ body-class: index
 		<h2 class="subhead">Direct importer and Curator of French and European Antiques in Houston, Texas</h2>
 	</div>
 </section>
+<section class="about-section" style="margin-bottom: 80px;">
+	<div class="row">
+		<div class="large-5 columns">
+			<img src="/img/index/about/betsy-austin.jpg" alt="Betsy Austin" title="Betsy Austin" class="box-shadow-1 border-radius-10 mb-24">
+			<div class="prose-wrap">
+				<h4 class="quote" style="margin: 0 auto; max-width: 350px;"></h4>
+			</div>
+		</div>
+	</div>
+</section>
 <section class="photo-gallery" style="margin-bottom: 120px;">
 	<div class="row row-of-5">
 		<div class="img-column"><img src="/img/galleries/1x1/1.jpg" alt=""></div>
