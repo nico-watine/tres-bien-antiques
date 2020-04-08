f80d2f1609b580e4d64e5fb5ad5e24e218728f5c
Nico Watine <nico@studioncreations.com>
2020-04-08 17:59:44 -0500

    Fix img-row of 5

--- src/pages/index.php
+++ src/pages/index.php
@@ -38,12 +38,12 @@ body-class: index
 	</div>
 </section>
 <section class="photo-gallery">
-	<div class="row medium-row-of-5">
+	<div class="row row-of-5">
 		<div class="img-column"><img src="/img/galleries/1x1/1.jpg" alt=""></div>
 		<div class="img-column"><img src="/img/galleries/1x1/2.jpg" alt=""></div>
 		<div class="img-column"><img src="/img/galleries/1x1/3.jpg" alt=""></div>
 		<div class="img-column"><img src="/img/galleries/1x1/4.jpg" alt=""></div>
-		<div class="img-column"><img src="/img/galleries/1x1/5.jpg" alt=""></div>
+		<div class="img-column hide-for-medium-only"><img src="/img/galleries/1x1/5.jpg" alt=""></div>
 	</div>
 	<div class="row collapse">
 		<div class="medium-6 columns"><img src="/img/galleries/4x3/1.jpg" alt=""></div>
