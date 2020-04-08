01df259d714807d29ddf9be4e0d06f36dac77a54
Nico Watine <nico@studioncreations.com>
2020-04-08 17:56:05 -0500

    Fix row-of-5 @small

--- src/pages/index.php
+++ src/pages/index.php
@@ -38,7 +38,7 @@ body-class: index
 	</div>
 </section>
 <section class="photo-gallery">
-	<div class="row row-of-5">
+	<div class="row medium-row-of-5">
 		<div class="img-column"><img src="/img/galleries/1x1/1.jpg" alt=""></div>
 		<div class="img-column"><img src="/img/galleries/1x1/2.jpg" alt=""></div>
 		<div class="img-column"><img src="/img/galleries/1x1/3.jpg" alt=""></div>
