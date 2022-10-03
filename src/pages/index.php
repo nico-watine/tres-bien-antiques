e3988fc124f5330298af0f7ea30974253f9e0bb2
Nico Watine <nico@studioncreations.com>
2022-10-03 15:00:06 -0500

    Add some text-shadow

--- src/pages/index.php
+++ src/pages/index.php
@@ -12,8 +12,8 @@ body-class: index
 	<div class="text-block">
 		<h1 class="headline">Très Bien Antiques</h1>
 		<h2 class="subhead">Direct importer and Curator of French and European Antiques in Houston,&nbsp;Texas</h2>
-		<div class="button-wrap" data-magellan data-threshold="0">
-			<a href="#main-content-section" class="button">Browse Online Catalog</a>
+		<div class="button-wrap">
+			<a href="https://shop.tb-antiques.com/" class="button">Browse Online Catalog</a>
 		</div>
 	</div>
 </section>
