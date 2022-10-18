c818427f3faf4c00c6fb5d45fffee6ca57930853
Nico Watine <nico@studioncreations.com>
2022-10-18 17:00:28 -0500

    Begin first card

--- src/pages/dev.php
+++ src/pages/dev.php
@@ -9,8 +9,15 @@ body-id:
 body-class:
 ---
 <section class="location-banner" style="padding-top: 40px;padding-bottom: 80px;">
-	<div class="row text-center">
+	<div class="row text-center" style="margin-bottom: 64px;">
 		<h2 class="font-weight-600 text-white text-uppercase">Locations</h2>
 		<h3 class="font-weight-600 text-white">Come visit us! We have 3 convenient locations.</h3>
+	</div>
+	<div class="row" style="max-width: 1200px;">
+		<div class="medium-4 columns">
+			<div class="locationCard text-center">
+				<h4 class="font-weight-bold">MAI</h4>
+			</div>
+		</div>
 	</div>
 </section>
