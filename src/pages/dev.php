e6ae7c23a964c292cf65ff34f98d6ffa2406985c
Nico Watine <nico@studioncreations.com>
2022-10-18 17:28:54 -0500

    Move some inlines to scss

--- src/pages/dev.php
+++ src/pages/dev.php
@@ -17,8 +17,8 @@ body-class:
 		<div class="medium-4 columns">
 			<div class="locationCard text-center">
 				<h4 class="font-weight-bold">MAI</h4>
-				<p style="letter-spacing: -0.2px;">Memorial Antiques & Interiors</p>
-				<p class="font-weight-600" style="letter-spacing: -0.2px;"><a href="https://g.page/MAIHouston">Design Center <br>
+				<p>Memorial Antiques & Interiors</p>
+				<p class="font-weight-600"><a href="https://g.page/MAIHouston">Design Center <br>
 				7026 Old Katy Rd #166 <br>
 				Houston, TX 77024</a></p>
 			</div>
@@ -26,16 +26,16 @@ body-class:
 		<div class="medium-4 columns">
 			<div class="locationCard text-center">
 				<h4 class="font-weight-bold">Marburger Farm Antique&nbsp;Show (Tent&nbsp;F)</h4>
-				<p style="letter-spacing: -0.2px;">During Round Top Antique Fair</p>
-				<p class="font-weight-600" style="letter-spacing: -0.2px;"><a href="https://goo.gl/maps/EwZAhZPdwmgSW4bGA">2248 South State Hwy&nbsp;237 <br>
+				<p>During Round Top Antique Fair</p>
+				<p class="font-weight-600"><a href="https://goo.gl/maps/EwZAhZPdwmgSW4bGA">2248 South State Hwy&nbsp;237 <br>
 				Round Top, TX 78954</a></p>
 			</div>
 		</div>
 		<div class="medium-4 columns">
 			<div class="locationCard text-center">
 				<h4 class="font-weight-bold">Our Warehouse</h4>
-				<p style="letter-spacing: -0.2px;">By appointment only</p>
-				<p class="font-weight-600" style="letter-spacing: -0.2px;"><a href="https://goo.gl/maps/wf2inyWYNKqmXxXH7">1237 Blalock Rd Ste&nbsp;#107 <br>
+				<p>By appointment only</p>
+				<p class="font-weight-600"><a href="https://goo.gl/maps/wf2inyWYNKqmXxXH7">1237 Blalock Rd Ste&nbsp;#107 <br>
 				Houston, TX 77055</a></p>
 			</div>
 		</div>
