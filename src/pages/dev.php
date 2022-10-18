244b12d285d477ca297568a0a8fcaaea083e5a84
Nico Watine <nico@studioncreations.com>
2022-10-18 17:09:47 -0500

    Edit other addresses

--- src/pages/dev.php
+++ src/pages/dev.php
@@ -27,18 +27,16 @@ body-class:
 			<div class="locationCard text-center">
 				<h4 class="font-weight-bold">Marburger Farm Antique&nbsp;Show (Tent&nbsp;F)</h4>
 				<p style="letter-spacing: -0.2px;">Memorial Antiques & Interiors</p>
-				<p class="font-weight-600" style="letter-spacing: -0.2px;"><a href="https://g.page/MAIHouston">Design Center <br>
-				7026 Old Katy Rd #166 <br>
-				Houston, TX 77024</a></p>
+				<p class="font-weight-600" style="letter-spacing: -0.2px;"><a href="https://goo.gl/maps/EwZAhZPdwmgSW4bGA">2248 South State Hwy 237 <br>
+				Round Top, TX 78954</a></p>
 			</div>
 		</div>
 		<div class="medium-4 columns">
 			<div class="locationCard text-center">
 				<h4 class="font-weight-bold">MAI</h4>
 				<p style="letter-spacing: -0.2px;">Memorial Antiques & Interiors</p>
-				<p class="font-weight-600" style="letter-spacing: -0.2px;"><a href="https://g.page/MAIHouston">Design Center <br>
-				7026 Old Katy Rd #166 <br>
-				Houston, TX 77024</a></p>
+				<p class="font-weight-600" style="letter-spacing: -0.2px;"><a href="https://goo.gl/maps/wf2inyWYNKqmXxXH7">1237 Blalock Rd Ste #107 <br>
+				Houston, TX 77055</a></p>
 			</div>
 		</div>
 	</div>
