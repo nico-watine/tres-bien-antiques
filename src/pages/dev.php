f963ea42149b326b2361733fc5676f627978a985
Nico Watine <nico@studioncreations.com>
2022-10-18 16:57:01 -0500

    Basic setup begun

--- src/pages/dev.php
+++ src/pages/dev.php
@@ -8,3 +8,9 @@ og-image:
 body-id:
 body-class:
 ---
+<section class="location-banner" style="padding-top: 40px;padding-bottom: 80px;">
+	<div class="row text-center">
+		<h2 class="font-weight-600 text-white text-uppercase">Locations</h2>
+		<h3 class="font-weight-600 text-white">Come visit us! We have 3 convenient locations.</h3>
+	</div>
+</section>
